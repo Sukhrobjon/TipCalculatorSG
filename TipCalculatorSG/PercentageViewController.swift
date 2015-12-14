ee362c1e4115ae1fe7ca99d87cf8c6a17b22f5fe
makhmudislamov <mislamov@mail.ccsf.edu>
2015-12-13 20:49:03 -0800

    ad percentageviewcontroller

---
+++ TipCalculatorSG/PercentageViewController.swift
@@ -0,0 +1,55 @@
+//
+//  PercentageViewController.swift
+//  TipCalculatorSG
+//
+//  Created by Sukhrobjon Golibboev on 12/13/15.
+//  Copyright © 2015 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class PercentageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("view did disappear")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
